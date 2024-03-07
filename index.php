202ac6e95f1f42ae0fa9b6f3f1415b45e1bbfce8
bagantegianluca <bagantegianluca@gmail.com>
2024-03-07 06:36:02 +0100

    Add index.php to generate a random password

---
+++ index.php
@@ -0,0 +1,50 @@
+<!-- 
+Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+Milestone 1
+Creare un form che invii in GET la lunghezza della password.
+Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+ -->
+
+<?php
+$passwordLenght = $_GET['passwordLenght'];
+var_dump($passwordLenght);
+
+function passwordGenerator($num)
+{
+    for ($i = 0; $i < $num; $i++) {
+        $randomAscii = rand(48, 122);
+        echo chr($randomAscii);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+
+    <!-- Import Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@latest/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-3">
+        <form action="" method="get">
+            <label for="passwordLenght">Lunghezza password</label>
+            <input type="number" name="passwordLenght" id="passwordLenght">
+            <button type="submit">Genera password</button>
+        </form>
+        <label for="password">Password generata</label>
+        <p id="password"><?= passwordGenerator($passwordLenght); ?></p>
+    </div>
+</body>
+
+</html>
